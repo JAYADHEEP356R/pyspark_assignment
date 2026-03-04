9fde562b54342c309d76de7461832f9fad319c5e
JAYADHEEP356R <jayadheep.r@diggibyte.com>
2026-03-04 22:53:45 +0530

    Config for python intrepretor

---
+++ src/Config/Initialize.py
@@ -0,0 +1,6 @@
+import  os
+
+
+def doinit():
+    os.environ['PYSPARK_PYTHON'] = r'C:\Users\raghu\PycharmProjects\Stages_Tasks\.venv\Scripts\python.exe'
+    os.environ['PYSPARK_DRIVER_PYTHON'] = r'C:\Users\raghu\PycharmProjects\Stages_Tasks\.venv\Scripts\python.exe'
